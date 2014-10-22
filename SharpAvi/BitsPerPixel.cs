526aa88d095c94b06663d6d2761d1048e6f9c5fd
Matthieu Penant <thieum22@hotmail.com>
2014-10-22 11:23:50 -0400

    Comment for 8bits greyscale.
    Comment for 8bits greyscale.

--- SharpAvi/BitsPerPixel.cs
+++ SharpAvi/BitsPerPixel.cs
@@ -3,7 +3,7 @@
     /// <summary>Number of bits per pixel.</summary>
     public enum BitsPerPixel
     {
-        /// <summary>8 bits per pixel.</summary>
+        /// <summary>8 bits per pixel. Greyscale is implied.</summary>
         Bpp8 = 8,
         /// <summary>16 bits per pixel.</summary>
         Bpp16 = 16,
