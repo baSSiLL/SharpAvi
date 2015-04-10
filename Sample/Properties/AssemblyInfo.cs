68764b4ceac1783abf9256e5c3acb6387494bdf0
Vasili Maslov <bassill@yandex.ru>
2015-04-10 14:19:45 +0300

    Updated assembly version. Added release notes to nuget package.

--- Sample/Properties/AssemblyInfo.cs
+++ Sample/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SharpAvi")]
-[assembly: AssemblyCopyright("Copyright © Vasili Maslov 2013-2014")]
+[assembly: AssemblyCopyright("Copyright © Vasili Maslov 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
