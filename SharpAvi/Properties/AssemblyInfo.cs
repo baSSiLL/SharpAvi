4baab756b74b87c98b9fb6248a8dd416aa038a1f
Vasili Maslov <bassill@yandex.ru>
2022-01-06 21:31:44 +0300

    Bumped the package version.

--- SharpAvi/Properties/AssemblyInfo.cs
+++ SharpAvi/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyFileVersion("2.2.1.0")]
